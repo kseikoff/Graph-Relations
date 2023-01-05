2d18445bcfbcdb12315243576c3bf9cf29f52d74
Alexey <thelegendofflaity@gmail.com>
2023-01-06 01:35:41 +0300

    simplified algorithm. program execution speed has been improved up to 43%

--- main.cpp
+++ main.cpp
@@ -69,59 +69,55 @@ int main() {
     }
 
     for (int i = 0; i < paths_container.size(); i++) {
-        if (paths_container.at(i).at(0) == paths_container.at(i).at(1)) {
-            reflexivity.at(paths_container.at(i).at(0) - 1) = 1;
-            count_zeroes_for_reflexivity -= 1;
-        }
-        vector<int> reversed_path;
-        reversed_path.reserve(2);
-        reversed_path.push_back(paths_container.at(i).at(1));
-        reversed_path.push_back(paths_container.at(i).at(0));
-        if (find(paths_container.begin(), paths_container.end(),
-                 reversed_path) != paths_container.end()) {
-            symmetric.at(i) = 1;
-            count_zeroes_for_symmetric -= 1;
-        }
         if (paths_container.at(i).at(0) != paths_container.at(i).at(1)) {
             for (int j = 0; j < paths_container.size(); j++) {
                 if (i != j) {
                     if (paths_container.at(i).at(1) == paths_container.at(j).at(0)) {
-                        if (paths_container.at(j).at(0) != paths_container.at(j).at(1)
-                            && (paths_container.at(i).at(0) != paths_container.at(j).at(1))) {
-                            vector<int> transitivity_path;
-                            transitivity_path.reserve(2);
-                            transitivity_path.push_back(paths_container.at(i).at(0));
-                            transitivity_path.push_back(paths_container.at(j).at(1));
-                            __gnu_cxx::__normal_iterator<vector<int> *,
-                                    vector<vector<int>>> path_presence = find(paths_container.begin(),
-                                                                          paths_container.end(),
-                                                                          transitivity_path);
-                            if (path_presence != paths_container.end()) {
-                                if (transitivity.at(i) == -1) {
-                                    transitivity.at(i) = 2;
-                                    deuce_presence = true;
-                                } else {
-                                    if (transitivity.at(i) != 2) {
-                                        transitivity.at(i) = 1;
-                                    }
-                                }
-                            } else if (transitivity.at(i) != 1 && transitivity.at(i) != 2) {
-                                transitivity.at(i) = -1;
-                            } else {
-                                if (transitivity.at(i) != 2) {
-                                    if (transitivity.at(i) != 0 && transitivity.at(i) != -1) {
+                        if (paths_container.at(j).at(0) != paths_container.at(j).at(1)) {
+                            if(paths_container.at(i).at(0) != paths_container.at(j).at(1)){
+                                vector<int> transitivity_path;
+                                transitivity_path.reserve(2);
+                                transitivity_path.push_back(paths_container.at(i).at(0));
+                                transitivity_path.push_back(paths_container.at(j).at(1));
+                                __gnu_cxx::__normal_iterator<vector<int> *,
+                                        vector<vector<int>>> path_presence = find(paths_container.begin(),
+                                                                                  paths_container.end(),
+                                                                                  transitivity_path);
+                                if (path_presence != paths_container.end()) {
+                                    if (transitivity.at(i) == -1) {
                                         transitivity.at(i) = 2;
                                         deuce_presence = true;
                                     } else {
-                                        transitivity.at(i) = -1;
+                                        if (transitivity.at(i) != 2) {
+                                            transitivity.at(i) = 1;
+                                        }
+                                    }
+                                } else if (transitivity.at(i) != 1 && transitivity.at(i) != 2) {
+                                    transitivity.at(i) = -1;
+                                } else {
+                                    if (transitivity.at(i) != 2) {
+                                        if (transitivity.at(i) != 0 && transitivity.at(i) != -1) {
+                                            transitivity.at(i) = 2;
+                                            deuce_presence = true;
+                                        } else {
+                                            transitivity.at(i) = -1;
+                                        }
                                     }
                                 }
+                            }
+                            else{
+                                symmetric.at(i) = 1;
+                                count_zeroes_for_symmetric -= 1;
                             }
                         }
                     }
                 }
             }
         } else {
+            symmetric.at(i) = 1;
+            count_zeroes_for_symmetric -= 1;
+            reflexivity.at(paths_container.at(i).at(0) - 1) = 1;
+            count_zeroes_for_reflexivity -= 1;
             if (transitivity.at(i) == 0) {
                 transitivity.at(i) = 3;
             }
