9a36caa7f4ef79faa15a58c01d2c0fae2302eea0
Alexey <thelegendofflaity@gmail.com>
2023-01-19 00:26:43 +0300

    com

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
 
     int edges_amount; int vertex_amount;
     bool overwrite = false; bool transitive = false; bool non_transitive = false;
-    int count_zeroes_for_symmetric; int count_zeroes_for_reflexivity;
+    int count_symmetric; int count_reflexivity;
 
     std::cin >> edges_amount >> vertex_amount;
     if (edges_amount == 0 && vertex_amount == 0) {
@@ -17,20 +17,17 @@ int main() {
         std::cout << "Граф без ребер антирефлексивен, остальные отношения любые" << std::endl; return 0;
     }
 
-    std::vector<std::vector<int>> paths_container(edges_amount);
-
+    std::vector<std::vector<int>> paths_container; paths_container.reserve(edges_amount);
     for (int i = 0; i < edges_amount; i++) {
         int path_from; int path_to;
         std::cin >> path_from >> path_to;
-        std::vector<int> path_values(2);
-        path_values[0] = path_from; path_values[1] = path_to;
-        paths_container[i] = path_values;
+        std::vector<int> path_values; path_values.reserve(2);
+        path_values.push_back(path_from); path_values.push_back(path_to);
+        paths_container.push_back(path_values);
     }
 
-    count_zeroes_for_symmetric = edges_amount;
-
-    count_zeroes_for_reflexivity = vertex_amount;
-
+    count_symmetric = edges_amount;
+    count_reflexivity = vertex_amount;
     std::vector<int> transitivity(edges_amount);
 
     for (int i = 0; i < paths_container.size(); i++) {
@@ -40,54 +37,50 @@ int main() {
                     if (paths_container.at(i).at(1) == paths_container.at(j).at(0)) {
                         if (paths_container.at(j).at(0) != paths_container.at(j).at(1)) {
                             if(paths_container.at(i).at(0) != paths_container.at(j).at(1)){
-                                std::vector<int> transitivity_path(2);
-                                transitivity_path[0] = paths_container.at(i).at(0);
-                                transitivity_path[1] = paths_container.at(j).at(1);
+                                std::vector<int> transitivity_path; transitivity_path.reserve(2);
+                                transitivity_path.push_back(paths_container.at(i).at(0));
+                                transitivity_path.push_back(paths_container.at(j).at(1));
                                 if (find(paths_container.begin(),paths_container.end(),
                                          transitivity_path) != paths_container.end()) {
                                     if (transitivity.at(i) == -1) {
-                                        transitivity.at(i) = 2;
-                                        overwrite = true;
+                                        transitivity.at(i) = 2; overwrite = true;
                                     } else if (transitivity.at(i) != 2) {
-                                            transitivity.at(i) = 1;
-                                            transitive = true;
+                                        transitivity.at(i) = 1; transitive = true;
                                     }
                                 } else {
-                                    if (transitivity.at(i) != 1 && transitivity.at(i) != 2) {
-                                        transitivity.at(i) = -1;
-                                        non_transitive = true;
+                                    if (transitivity.at(i) == 1) {
+                                        transitivity.at(i) = 2; overwrite = true;
                                     }
-                                    else {
-                                        transitivity.at(i) = 2;
-                                        overwrite = true;
+                                    else if (transitivity.at(i) != 2){
+                                        transitivity.at(i) = -1; non_transitive = true;
                                     }
                                 }
                             }
-                            else{
-                                count_zeroes_for_symmetric -= 1;
+                            else {
+                                count_symmetric -= 1;
                             }
                         }
                     }
                 }
             }
         } else {
-            count_zeroes_for_symmetric -= 1;
-            count_zeroes_for_reflexivity -= 1;
+            count_symmetric -= 1;
+            count_reflexivity -= 1;
             if (transitivity.at(i) == 0) {
                 transitivity.at(i) = 3;
             }
         }
     }
-    if (count_zeroes_for_symmetric == 0) {
+    if (count_symmetric == 0) {
         std::cout << "Симметричный" << std::endl;
-    } else if (count_zeroes_for_symmetric == edges_amount) {
+    } else if (count_symmetric == edges_amount) {
         std::cout << "Антисимметричный" << std::endl;
     } else {
         std::cout << "Несимметричный" << std::endl;
     }
-    if (count_zeroes_for_reflexivity == 0) {
+    if (count_reflexivity == 0) {
         std::cout << "Рефлексивный" << std::endl;
-    } else if (count_zeroes_for_reflexivity == vertex_amount) {
+    } else if (count_reflexivity == vertex_amount) {
         std::cout << "Антирефлексивный" << std::endl;
     } else {
         std::cout << "Нерефлексивный" << std::endl;
